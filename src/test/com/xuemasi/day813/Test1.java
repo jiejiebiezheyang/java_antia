c31b288ad75dbdf0765f55e8a1a08d25b24de0db
antia <ltpcloud@qq.com>
2022-08-13 15:40:14 +0800

    update Test

---
+++ src/test/com/xuemasi/day813/Test1.java
@@ -0,0 +1,84 @@
+package com.xuemasi.day813;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: ltpcloud@qq.com
+ * @Date: 2022/08/13/14:42
+ * @Description:
+ */
+
+/*
+ * 抽象类、继承、接口综合 设计一个系统 XXX门的实现过程
+ * 流程
+ * 设计一张抽象的门Door 那么对于这张门来说 就应该拥有所有门的共性 开门openDoor()和关门closeDoor()
+ * 然后对门进行另外的功能设计,防盗–theftProof()、防水–waterproof()、防弹–bulletproof()、防火、防锈……
+ * 要求 利用继承、抽象类、接口的知识设计该门
+ * */
+abstract class Door {
+    double height;
+    double width;
+
+    public Door(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    // 开门
+    abstract void openDoor();
+
+    // 关门
+    abstract void closeDoor();
+}
+
+interface powerfulDoor {
+    void theftProof();
+
+    void waterproof();
+
+    void bulletproof();
+
+}
+
+class Adoor extends Door implements powerfulDoor {
+
+    public Adoor(double height, double width) {
+        super(height, width);
+    }
+
+    @Override
+    void openDoor() {
+        System.out.println("开门");
+    }
+
+    @Override
+    void closeDoor() {
+        System.out.println("关门");
+    }
+
+    @Override
+    public void theftProof() {
+        System.out.println("防盗");
+    }
+
+    @Override
+    public void waterproof() {
+        System.out.println("防水");
+    }
+
+    @Override
+    public void bulletproof() {
+        System.out.println("防弹");
+    }
+}
+
+public class Test1 {
+    public static void main(String[] args) {
+        Adoor door = new Adoor(2, 1);
+        door.openDoor();
+        door.closeDoor();
+        door.bulletproof();
+        door.theftProof();
+        door.waterproof();
+    }
+}
---
+++ src/test/com/xuemasi/day813/Test3.java
@@ -0,0 +1,91 @@
+package com.xuemasi.day813;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: ltpcloud@qq.com
+ * @Date: 2022/08/13/14:59
+ * @Description:
+ */
+
+/*
+ * 构造方法的重载
+ * 定义一个名为Vehicles 交通工具 的基类
+ *      该类中应包含String类型的成员属性brand 商标 和color 颜色 还应包含成员方法run 行驶 在控制台显示“我已经开动了”
+ *      和showInfo 显示信息 在控制台显示商标和颜色 并编写构造方法初始化其成员属性。
+ * 编写Car 小汽车 类继承于Vehicles类 增加int型成员属性seats 座位 还应增加成员方法showCar 在控制台显示小汽车的信息 并编写构造方法
+ * 编写Truck 卡车 类继承于Vehicles类 增加float型成员属性load 载重 还应增加成员方法showTruck 在控制台显示卡车的信息 并编写构造方法
+ * 在main方法中测试以上各类。
+ * */
+class Vehicles {
+    String brand;
+    String color;
+
+    public Vehicles(String brand, String color) {
+        this.brand = brand;
+        this.color = color;
+    }
+
+    public void run() {
+        System.out.println("我已经开动了");
+    }
+
+    public void showInfo() {
+        System.out.println("车辆{" +
+                "品牌=" + brand +
+                ", 颜色=" + color +
+                "}");
+    }
+}
+
+
+class Car2 extends Vehicles {
+    int seat;
+
+    public Car2(String brand, String color, int seat) {
+        super(brand, color);
+        this.seat = seat;
+    }
+
+    public void showCar() {
+        System.out.println("小汽车{" +
+                "座位=" + seat +
+                ", 品牌=" + brand +
+                ", 颜色=" + color +
+                "}");
+    }
+}
+
+
+class Truck extends Vehicles {
+    float load;
+
+    public Truck(String brand, String color, float load) {
+        super(brand, color);
+        this.load = load;
+    }
+
+    public void showTruck() {
+        System.out.println("卡车{" +
+                "载重=" + load +
+                ", 品牌=" + brand +
+                ", 颜色=" + color +
+                "}");
+    }
+}
+
+public class Test3 {
+    public static void main(String[] args) {
+        Car2 car = new Car2("奔驰", "红", 5);
+        car.run();
+        car.showInfo();
+        car.showCar();
+
+        System.out.println();
+
+        Truck truck = new Truck("三一", "白", 25f);
+        truck.run();
+        truck.showInfo();
+        truck.showTruck();
+    }
+}
